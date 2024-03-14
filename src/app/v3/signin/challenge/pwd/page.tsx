adb67099fcf44d4c9fca1d6a237959d882663152
Genorred <gmihasik@gmail.com>
2024-03-14 17:02:29 +0200

    i do not understand why

---
+++ src/app/v3/signin/challenge/pwd/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {redirect} from "next/navigation";
+
+const Page = () => {
+    redirect('/v3/signin/challenge/pwd/TL=ADg0xRopM7G9xKUmq8EbyDwsffyN88ne17_CTZVwoP1Qw98Yh9EritiBKeZN&checkConnection&checkedDomains&cid=&continue=https%3A%2F%2Fwww.google.com.ua%2F&dsh=S5255428%3A1710420788140155&ec=GAlAmgQ&flowEntry=AddSession&flowName=GlifWebSignIn&hl=ru&pstMsg=0&theme=mn&authuser=0')
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Page;
